a7ffb9d5ff2013dc77a550fa88d5794699d0bbc2
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-06-25 20:01:30 +1000

    Add daily pick functionality
    feat(front-end): crushLanding.html <- [crushLanding.css, scrollTo.js]
    --------------------------------------------------------------
    Added: /CSS + new HTML internal elements/
    [pickGallery, pickCard, pickable, todaysImg, todaysCnt, todaysTitle]
    
    scrollTo.js 
    Additions: /translation function set on time interval/

---
+++ scrollTo.js
@@ -0,0 +1,20 @@
+"use strict";
+let today = document.getElementById('today');
+
+function scrollToday() {
+    window.scrollTo({top:1500, behavior:'smooth'});
+    console.log("scrolled to 'today' ")
+}
+
+let pickable = document.querySelector('.pickable');
+
+setInterval(topPickScroll, 5000);
+function topPickScroll() {
+    pickable.scrollLeft += 440;
+    /*console.log("Top Choice window scrolled");*/
+}
+setInterval(topPickRefresh, 25000);
+function topPickRefresh() {
+    pickable.scrollLeft -= 2200;
+    /*console.log('Top Choice refreshed'); */
+}
