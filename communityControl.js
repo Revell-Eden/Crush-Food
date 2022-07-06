8c85e1e434872e66183166f687a0176bc8e6386d
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-07-06 12:16:31 +1000

    Add Comment tracker function
    Add function that tracks all comments made by the user and appends it as an entry in  users localstorage;

--- communityControl.js
+++ communityControl.js
@@ -1,4 +1,8 @@
 "use strict";
+let preStore = JSON.parse(localStorage.getItem("allEntries"));
+window.onload = function commentHlp() {
+    console.log("Comment Count:", preStore.length);
+}
 
 let recipe = document.querySelector('.recipePreview');
 let recipeBtn = document.querySelector('.recipe');
@@ -15,7 +19,21 @@ function showRecipe() {
         console.log("close");
     }
 }
-
-function newComment() {
+function postComment() {
     
+    if (preStore == null) preStore = [];
+    let pushedData = document.getElementById('newComment').value;
+    /*let userId = document.getElementById('userId').value;*/
+    let entry = {/*"userID": userId,*/ "comment": pushedData};
+    /*
+    preStore.push({comment: pushedData});
+    preStore.forEach((item,i) => {
+        item.id = i + 1;
+        return item
+    });*/
+    localStorage.setItem('entry', JSON.stringify(entry));
+    // Save allENtries back to local storage //
+    preStore.push(entry);
+    localStorage.setItem("allEntries", JSON.stringify(preStore));
+    console.log(preStore);
 }
