f149f965fde80fa97790c9a8c92af5df346dfe33
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-07-06 14:35:45 +1000

    Update Comment local storage method

--- communityControl.js
+++ communityControl.js
@@ -1,9 +1,3 @@
-"use strict";
-let preStore = JSON.parse(localStorage.getItem("allEntries"));
-window.onload = function commentHlp() {
-    console.log("Comment Count:", preStore.length);
-}
-
 let recipe = document.querySelector('.recipePreview');
 let recipeBtn = document.querySelector('.recipe');
 function showRecipe() {
@@ -19,8 +13,17 @@ function showRecipe() {
         console.log("close");
     }
 }
+// Extend Comment Menu //
+let commentSection = document.querySelector('.commentSection');
+setInterval(extendComment(), 3000);
+function extendComment() {    
+    commentSection.scrollLeft -= 2000;
+    console.log('scrolled');
+};
+
+
+let preStore = JSON.parse(localStorage.getItem("allEntries"));
 function postComment() {
-    
     if (preStore == null) preStore = [];
     let pushedData = document.getElementById('newComment').value;
     /*let userId = document.getElementById('userId').value;*/
@@ -31,9 +34,25 @@ function postComment() {
         item.id = i + 1;
         return item
     });*/
+
     localStorage.setItem('entry', JSON.stringify(entry));
-    // Save allENtries back to local storage //
+    // Save allEntries back to local storage //
     preStore.push(entry);
     localStorage.setItem("allEntries", JSON.stringify(preStore));
     console.log(preStore);
+
+    // Comment time-stamp //
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    const commentDate = today.toLocaleDateString();
+
+    // Add new comment instance with prePost data entered //
+    document.querySelector('.commentSection').insertAdjacentHTML(
+        'afterBegin',
+        `<li class="commented">
+        ${pushedData}
+        <span>${commentDate}</span>
+        </li>`
+    )
+    console.log("added");
 }
