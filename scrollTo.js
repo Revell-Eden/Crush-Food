aa64709dcd9bd3d92b8da7473b2ff22c78b74457
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-07-06 23:30:54 +1000

    Add files via upload

--- scrollTo.js
+++ scrollTo.js
@@ -1,17 +1,19 @@
 "use strict";
 let today = document.getElementById('today');
-
 function scrollToday() {
     window.scrollTo({top:1500, behavior:'smooth'});
     console.log("scrolled to 'today' ")
 }
-
 let pickable = document.querySelector('.pickable');
-/*
-setInterval(topPickScroll, 1000);
 
+setInterval(topPickScroll, 5000);
 function topPickScroll() {
-    pickable.scrollLeft += 440;
-    console.log("Top Choice window scrolled");
-    console.log('scrolled');
+    pickable.scrollLeft += 430;
+    /*console.log("Top Choice window scrolled");*/
+}
+
+setInterval(topPickRefresh, 25000);
+function topPickRefresh() {
+    pickable.scrollLeft -= 2200;
+    /*console.log('Top Choice refreshed');*/
 }
